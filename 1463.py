0ff7217459bf878a5d42c0d42ecce50adc8fede8
sidamodev <sidamo.dev@gmail.com>
2024-03-04 14:15:57 +0900

    add: solved 1463 by dfs

---
+++ 1463.py
@@ -0,0 +1,21 @@
+N = int(input())
+
+min_cnt = 0xffff
+
+def solve(n, X):
+    global min_cnt
+    if n >= min_cnt:
+        return
+    if X == 1:
+        if n < min_cnt:
+            min_cnt = n
+        return
+
+    if X % 3 == 0:
+        solve(n + 1, X / 3)
+    if X % 2 == 0:
+        solve(n + 1, X / 2)
+    solve(n + 1, X - 1)
+
+solve(0, N)
+print(min_cnt)
