8d5f5a5f8ebf595de8b7b7068093b84e68f456c3
sidamodev <sidamo.dev@gmail.com>
2024-03-22 16:53:59 +0900

    add: 2206

--- 2206_wall_break1.py
+++ 2206_wall_break1.py
@@ -1,38 +1,32 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 d_ij = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
-def bfs(s_i, s_j, w_b):
-    q = deque([(s_i, s_j)])
-    visit[s_i][s_j] = 1
+def bfs(s_i, s_j):
+    q = deque([(s_i, s_j, 1, 1)])
+    v1[0][0] = 1
     while q:
-        i, j = q.popleft()
+        i, j, b, d = q.popleft()
+        if (i, j) == (N - 1, M - 1):
+            return d
         for di, dj in d_ij:
             ni, nj = i + di, j + dj
             if 0 <= ni < N and 0 <= nj < M:
-                if not visit[ni][nj] and A[ni][nj] == 0:
-                    visit[ni][nj] = visit[i][j] + 1
-                    q.append((ni, nj))
-                    print(visit)
-
-            # elif w_b and not visit[ni][nj] and A[ni][nj] == 1:
-            #     bfs(ni, nj, 0)
-
-    # tmp = visit[N - 1][M - 1]
-    # if tmp > 0:
-    #     v.append(tmp)
+                if not b and not v2[ni][nj] and not v1[ni][nj] and A[ni][nj] == '0':
+                    v2[ni][nj] = d + 1
+                    q.append((ni, nj, 0, d + 1))
+                if b and not v1[ni][nj]:
+                    v1[ni][nj] = d + 1
+                    if A[ni][nj] == '1':
+                        q.append((ni, nj, 0, d + 1))
+                    else:
+                        q.append((ni, nj, 1, d + 1))
+    return -1
 
 
 N, M = map(int, input().split())
-A = [list(map(int, input().strip())) for _ in range(N)]
-v = []
-visit = [[0] * M for _ in range(N)]
-
-bfs(0, 0, 1)
-res = -1
-if v:
-    res = min(v) + 1
-print(v)
+A = [input().strip() for _ in range(N)]
+v1, v2 = [[0] * M for _ in range(N)], [[0] * M for _ in range(N)]
+print(bfs(0, 0))
