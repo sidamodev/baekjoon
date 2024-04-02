965e6ab4c4e0592fbb578a5254a1c5f3e9b76d2a
sidamodev <sidamo.dev@gmail.com>
2024-04-02 15:09:50 +0900

    fix: solved 14052_lab

--- 14052_lab_1.py
+++ 14052_lab_1.py
@@ -1,28 +1,56 @@
 from collections import deque
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
 d_ij = ((0, 1), (0, -1), (1, 0), (-1, 0))
 
 
-def dfs(lev):
+def dfs(lev, a, arr):
+    global min_v
     if lev == 3:
+        cnt_v = 0
+        lab = v = [ x[:] for x in arr ]
+        for i in range(N):
+            for j in range(M):
+                if lab[i][j] == 1:
+                    cnt_v += 1
+                elif lab[i][j] == 2:
+                    cnt = 0
+                    queue = deque([(i, j)])
+                    while queue:
+                        x, y = queue.popleft()
+                        for di, ij in d_ij:
+                            ni, nj = x + di, y + ij
+                            if 0 <= ni < N and 0 <= nj < M:
+                                if lab[ni][nj] == 0:
+                                    lab[ni][nj] = 2
+                                    cnt += 1
+                                    if cnt >= min_v: return
+                                    queue.append((ni, nj))
+                    cnt_v += cnt
+                    if cnt_v >= min_v: return
+        if cnt_v < min_v:
+            min_v = cnt_v
         return
-
-
-def bfs(p, q):
-    queue = deque([(p, q)])
-    while queue:
-        x, y = queue.popleft()
-        for di, ij in d_ij:
-            ni, nj = x + di, y + ij
-            if 0 <= ni < N and 0 <= nj < M:
-                if A[ni][nj] == 0:
-                    A[ni][nj] = 2
-                    queue.append((ni, nj))
+    for idx in range(a, N * M):
+        i = idx // M
+        j = idx - i * M
+        if arr[i][j] == 0:
+            arr[i][j] = 1
+            dfs(lev + 1, idx + 1, arr)
+            arr[i][j] = 0
 
 
 N, M = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(N)]
-
+init_v = 0
 for i in range(N):
     for j in range(M):
         if A[i][j] == 2:
-            bfs(i, j)
+            init_v += 1
+min_v = 64
+dfs(0, 0, A)
+result = M * N - min_v - init_v
+print(str(result))
