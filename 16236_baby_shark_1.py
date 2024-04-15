9d72342a2f798400ef241a53c4a3d2dd87b0dc5f
sidamodev <sidamo.dev@gmail.com>
2024-04-15 23:08:09 +0900

    fix: 16236 using dijkstra

--- 16236_baby_shark_1.py
+++ 16236_baby_shark_1.py
@@ -1,33 +1,43 @@
-from collections import deque
+from heapq import heappush, heappop
 
 d_ij = ((-1, 0), (0, -1), (0, 1), (1, 0))
+
+
+def solve(baby_i, baby_j):
+    baby_size = eat_count = 2
+    time = 0
+    pq = [(0, baby_i, baby_j)]
+    visit = [[0] * N for _ in range(N)]
+    while pq:
+        d, baby_i, baby_j = heappop(pq)
+        # 진행한 칸이 먹이인 경우
+        if 0 < A[baby_i][baby_j] < baby_size:
+            A[baby_i][baby_j] = 0
+            eat_count -= 1
+            time += d
+            if eat_count == 0:
+                baby_size += 1
+                eat_count = baby_size
+            # 초기화
+            d = 0
+            pq = []
+            visit = [[0] * N for _ in range(N)]
+        for di, dj in d_ij:
+            baby_ni, baby_nj = baby_i + di, baby_j + dj
+            # 조건 필터링 -> 맵 범위 / 방문체크 / 사이즈
+            if not (0 <= baby_ni < N and 0 <= baby_nj < N): continue
+            if visit[baby_ni][baby_nj]: continue
+            if A[baby_ni][baby_nj] > baby_size: continue
+            visit[baby_ni][baby_nj] = 1
+            heappush(pq, (d + 1, baby_ni, baby_nj))
+    return time
+
+
 N = int(input())
 A = [list(map(int, input().split())) for _ in range(N)]
-v = [[0] * N for _ in range(N)]
-size = exp = 2
-pos_i = pos_j = t = 0
 for i in range(N * N):
     if A[i // N][i % N] == 9:
-        pos_i, pos_j = i // N, i % N
+        init_i, init_j = i // N, i % N
+        A[i // N][i % N] = 0
         break
-
-queue = deque([(pos_i, pos_j)])
-while queue:
-    i, j = queue.popleft()
-    print(i, j)
-    for di, dj in d_ij:
-        ni, nj = i + di, j + dj
-        if not (0 <= ni < N and 0 <= nj < N) or (A[ni][nj] > size): continue
-        v[ni][nj] = 1
-        if A[ni][nj] == 0 or A[ni][nj] == size:
-            t += 1
-            queue.append((ni, nj))
-        elif A[ni][nj] < size:
-            A[ni][nj] = 0
-            t += 1
-            exp -= 1
-            if exp == 0:
-                size += 1
-                exp = size
-            queue.append((ni, nj))
-            break
+print(solve(init_i, init_j))
