4b244e577e051156a2ee0bb9b028998ded4c9a00
sidamodev <sidamo.dev@gmail.com>
2024-04-19 17:48:57 +0900

    fix: 1956_skeleton

--- 1956_exercise.py
+++ 1956_exercise.py
@@ -1,8 +1,34 @@
 from heapq import heappush, heappop
 
+
+def dijkstra(start_v):
+    visit = set()
+    pq = [(0, start_v)]
+    w_acc = 0
+    while pq:
+        w, v = heappop(pq)
+        w_acc += w
+        if w and v == start_v: return w_acc
+        if v in visit: break
+        visit.add(v)
+        for nv, nw in adj[v]:
+            heappush(pq, (nw, nv))
+
+
 V, E = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(E)]
+g = [list(map(int, input().split())) for _ in range(E)]
+adj = [[] for _ in range(V + 1)]
 
-pq = [(0)]
-while pq:
+for s, e, w in g:
+    adj[s].append((e, w))
 
+dist_lst = []
+for i in range(1, V + 1):
+    if not adj[i]: continue
+    tmp = dijkstra(i)
+    if tmp: heappush(dist_lst, tmp)
+
+result = -1
+if dist_lst:
+    result = heappop(dist_lst)
+print(result)
