2cde3200d48124d5998f4a24bce381de6fa689c1
sidamodev <sidamo.dev@gmail.com>
2024-06-15 22:20:22 +0900

    fix: 21610_shark_rain structure enhanced

--- 21610_shark_rain.py
+++ 21610_shark_rain.py
@@ -6,73 +6,28 @@ N, M = input_list()
 A = [list(input_list()) for _ in range(N)]
 move = [list(input_list()) for _ in range(M)]
 
+d_ij = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
+cloud = [(N - 1, 0), (N - 1, 1), (N - 2, 0), (N - 2, 1)]
 
-class Cloud:
-    move_arr = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
-
-    def __init__(self):
-        self.cloud = [[0] * N for _ in range(N)]
-        self.cloud[N - 1][0] = 1
-        self.cloud[N - 1][1] = 1
-        self.cloud[N - 2][0] = 1
-        self.cloud[N - 2][1] = 1
-
-    def move(self, direction, dist):
-        tmp_arr = [[0] * N for _ in range(N)]
-        for i in range(N):
-            for j in range(N):
-                if self.cloud[i][j] == 0: continue
-                ni, nj = i, j
-                for k in range(dist):
-                    ni, nj = ni + Cloud.move_arr[direction - 1][0], nj + Cloud.move_arr[direction - 1][1]
-                    if ni < 0:
-                        ni = N - 1
-                    elif ni >= N:
-                        ni = 0
-                    if nj < 0:
-                        nj = N - 1
-                    elif nj >= N:
-                        nj = 0
-                tmp_arr[ni][nj] = 1
-        self.cloud = [x[:] for x in tmp_arr]
-
-    def make(self, rained):
-        for i in range(N):
-            for j in range(N):
-                if A[i][j] >= 2 and (i, j) not in rained:
-                    A[i][j] -= 2
-                    self.cloud[i][j] = 1
-
-    def remove(self):
-        self.cloud = [[0] * N for _ in range(N)]
-
-
-def rain(cloud):
-    rained = []
-    for i in range(N):
-        for j in range(N):
-            if cloud[i][j]:
-                rained.append((i, j))
-                A[i][j] += 1
-    return rained
-
-
-def copy_bug(rain_arr):
-    for i, j in rain_arr:
-        diag_water = 0
+for d, s in move:
+    prev_cloud = set()
+    for cloud_i, cloud_j in cloud:
+        ni = (cloud_i + d_ij[d - 1][0] * s) % N
+        nj = (cloud_j + d_ij[d - 1][1] * s) % N
+        A[ni][nj] += 1
+        prev_cloud.add((ni, nj))
+    cloud = []
+    for i, j in prev_cloud:
+        diag_sum = 0
         for ni, nj in [(i - 1, j - 1), (i - 1, j + 1), (i + 1, j - 1), (i + 1, j + 1)]:
             if not (0 <= ni < N and 0 <= nj < N): continue
             if A[ni][nj]:
-                diag_water += 1
-        A[i][j] += diag_water
-
-
-c = Cloud()
-for d, s in move:
-    c.move(d, s)
-    cloud_regin = rain(c.cloud)
-    c.remove()
-    copy_bug(cloud_regin)
-    c.make(cloud_regin)
+                diag_sum += 1
+        A[i][j] += diag_sum
+    for i in range(N):
+        for j in range(N):
+            if A[i][j] >= 2 and (i, j) not in prev_cloud:
+                A[i][j] -= 2
+                cloud.append((i, j))
 
 print(sum(map(sum, A)))
