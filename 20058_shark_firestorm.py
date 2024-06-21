c9e49506c103b52cd5c041fc0169ba760498c813
sidamodev <sidamo.dev@gmail.com>
2024-06-21 16:16:29 +0900

    add: 20058

--- 20058_shark_firestorm.py
+++ 20058_shark_firestorm.py
@@ -3,5 +3,9 @@ A = [list(map(int, input().split())) for _ in range(2 ** N)]
 L = list(map(int, input().split()))
 
 for level in L:
-    size = (2 ** N) ** 2 // (2 ** level) ** 2
-
+    total_grid = (2 ** N) ** 2
+    sub_grid = (2 ** level) ** 2
+    sub_total = total_grid // sub_grid
+    # sub_line = int(sub_total ** 0.5)
+    for i in range(sub_total):
+        print(i, i//sub_)
